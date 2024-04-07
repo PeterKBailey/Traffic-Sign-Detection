b648bceaade09804041afa3fd008052119a21e47
HamzaOsman <hamzaosman001@gmail.com>
2024-04-07 13:24:05 -0400

    Added VGNet inspired NN

--- Neural-Net/main.py
+++ Neural-Net/main.py
@@ -1,14 +1,22 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 import os
 from PIL import Image
 import numpy as np
-from net import AlexNet, LeNet
+from net import LeNet, VGNet
 import pandas as pd
 import matplotlib.pyplot as plt
+from torch.utils.mobile_optimizer import optimize_for_mobile
 
-def preProcessData(directory_path, data_type="train"):
+NUM_CLASSES = 43
+IMG_HEIGHT = 32
+IMG_WIDTH = 32
+NUM_EPOCHS = 20
+
+def preProcessData2(directory_path, data_type="train"):
     trainset_X = []
     trainset_Y = []
 
@@ -37,8 +45,6 @@ def preProcessData(directory_path, data_type="train"):
         trainset_Y = pd.read_csv(f"{directory_path}/GT-final_test.csv", sep=";", usecols=[7]).to_numpy()
 
     trainset_X = np.array(trainset_X) / 255.
-    trainset_Y = np.array(trainset_Y).reshape(-1,1)
-
     tensor_X = torch.Tensor(trainset_X)
     tensor_X = tensor_X.permute(0, 3, 1, 2)
     tensor_y = torch.LongTensor(trainset_Y)
@@ -46,6 +52,30 @@ def preProcessData(directory_path, data_type="train"):
     dataset = torch.utils.data.TensorDataset(tensor_X, tensor_y)
 
     return dataset
+
+def preProcessData():
+    def processDataset(dataset):
+        X = []
+        Y = []
+        for image, label in dataset:
+            X.append(image)
+            Y.append(label)
+
+        X = torch.stack(X)
+        Y = torch.LongTensor(Y)
+        return torch.utils.data.TensorDataset(X, Y)
+    
+    transform = transforms.Compose([
+        transforms.Resize((IMG_WIDTH, IMG_HEIGHT)),  
+        transforms.ToTensor(),
+    ])
+    
+    train_data = datasets.GTSRB(root="data2", split="train", download=True, transform=transform)
+    test_data = datasets.GTSRB(root="data2", split="test", download=True, transform=transform)
+    
+    train_dataset = processDataset(train_data)
+    test_dataset = processDataset(test_data)
+    return train_dataset, test_dataset
 
 # Training loop
 def train(model, optimizer, train_dataloader):
@@ -55,7 +85,7 @@ def train(model, optimizer, train_dataloader):
         data, target = data.to(device), target.to(device)  # move data to the same device
         optimizer.zero_grad()  # clear existing gradients
         output = model(data)  # forward pass
-        target = target.view(-1)
+        #target = target.view(-1)
         loss = criterion(output, target)  # compute the loss
         loss.backward()  # backward pass: calculate the gradients
         optimizer.step()  # take a gradient step
@@ -74,25 +104,27 @@ def test(net, test_dataloader):
             total += labels.size(0)
 
             if CUDA:
-                correct += (predicted.cpu()==labels.cpu()).sum().item()
+                correct += torch.eq(predicted.cpu(), labels.cpu()).sum().item()
             else:
-                correct += torch.eq(predicted.unsqueeze(1), labels).sum().item()
+                correct += torch.eq(predicted, labels.cpu()).sum().item()
 
     test_accuracy = 100 * (correct / total)
     return test_accuracy
 
-def learn(train_dataloader, test_dataloader, learning_rate=0.001, momentum=0.9):
-    net = LeNet(NUM_CLASSES)
+def learn(train_dataloader, test_dataloader, learning_rate=0.001, momentum=0.9, dropout=0.75):
+    net = VGNet(NUM_CLASSES, dropout)
+    net = net.to(device)
     optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
     accuracy_list = []
 
     for epoch in range(NUM_EPOCHS):
-        net = train(net, optimizer,train_dataloader)
-        acc = test(net, test_dataloader)
-        accuracy_list.append(acc)
-        print('Epoch=%d Test Accuracy=%.3f' % (epoch + 1, acc))
+        net = train(net, optimizer, train_dataloader)
+        test_acc = test(net, test_dataloader)
+        train_acc = test(net, train_dataloader)
+        accuracy_list.append(test_acc)
+        print('Epoch=%d Train Accuracy=%.3f Test Accuracy=%.3f' % (epoch + 1, train_acc, test_acc))
 
-    return accuracy_list
+    return net, accuracy_list
 
 def plotAccuracies(accuracies, labels):
     accuracies = np.array(accuracies).reshape(len(labels), -1)
@@ -110,10 +142,11 @@ def plotAccuracies(accuracies, labels):
 
 def runExperiments():
     #Hyper params
-    batch_sizes = [1, 4, 16, 64]
+    batch_sizes = [16, 32, 64]
     learning_rates = [0.0001, 0.001, 0.01, 0.1]
-    momentums = [0.5, 0.75, 0.9, 0.99]
-
+    momentums = [0.5, 0.75, 0.9, 1]
+    dropouts = [0, 0.1, 0.25, 0.5, 0.75]
+    
     # Test batch sizes
     print("\nTesting batch sizes...")
     accuracies = []
@@ -122,13 +155,14 @@ def runExperiments():
         print(f'Batch-size: {value}')
         train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=value, shuffle=True)
         test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=64)
-        accuracies += learn(train_dataloader, test_dataloader)
+        _, acc = learn(train_dataloader, test_dataloader)
+        accuracies+= acc
         labels.append(f'Batch size: {value}')
 
     plt.title('GTSRB Batch Sizes')
     plotAccuracies(accuracies, labels)
 
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True)
     test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=64)
 
     # Test learning rates
@@ -137,7 +171,8 @@ def runExperiments():
     labels = []
     for _, value in enumerate(learning_rates):
         print(f'LR: {value}')
-        accuracies += learn(train_dataloader, test_dataloader, learning_rate=value)
+        _, acc = learn(train_dataloader, test_dataloader, learning_rate=value)
+        accuracies+= acc
         labels.append(f'LR: {value}')
 
     plt.title('GTSRB Learning Rates')
@@ -149,24 +184,76 @@ def runExperiments():
     labels = []
     for _, value in enumerate(momentums):
         print(f'Momentum: {value}')
-        accuracies += learn(train_dataloader, test_dataloader, momentum=value)
+        _, acc = learn(train_dataloader, test_dataloader, momentum=value)
+        accuracies+= acc
         labels.append(f'Momentum: {value}')
 
     plt.title('GTSRB Momentum')
     plotAccuracies(accuracies, labels)
 
+    # Test dropouts
+    print("\nTesting dropouts...")
+    accuracies = []
+    labels = []
+    for _, value in enumerate(dropouts):
+        print(f'Dropout: {value}')
+        _, acc = learn(train_dataloader, test_dataloader, dropout=value)
+        accuracies+= acc
+        labels.append(f'Dropout: {value}')
+
+    plt.title('GTSRB Dropout')
+    plotAccuracies(accuracies, labels)
+
+def runExperiments2():
+    #Hyper params
+    learning_rates = [0.0001, 0.001]
+    momentums = [0.5, 0.75, 0.9]
+    dropouts = [0.1, 0.25, 0.5, 0.75] 
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=64)
+
+    for _, lr in enumerate(learning_rates):
+        for _, m in enumerate(momentums):
+            for _, d in enumerate(dropouts):
+                print(f'\nLR: {lr}, M: {m}, D: {d}')
+                learn(train_dataloader, test_dataloader, learning_rate=lr, momentum=m, dropout=d)
+
+def createModel(batch_size, learning_rate, momentum, dropout):
+    print("Creating model...")
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=64)
+    model, _ = learn(train_dataloader, test_dataloader, learning_rate=learning_rate, momentum=momentum, dropout=dropout)
+    model = model.cpu()
+    #torch.save(model.state_dict(), "models/model_scripted.pt")
+
+def optimizeModel():
+    model = VGNet(NUM_CLASSES)
+    model.load_state_dict(torch.load('models/model_scripted.pt',map_location="cpu"))
+    model.eval()
+    example = torch.rand(1, 3, IMG_WIDTH, IMG_HEIGHT)
+    traced_script_module = torch.jit.trace(model, example)
+    optimized_traced_model = optimize_for_mobile(traced_script_module)
+    #optimized_traced_model._save_for_lite_interpreter("model.pt")
+
+def loadData(train_path='data/gtsrb/train.pth', test_path='data/gtsrb/test.pth'):
+    print("Loading data...")
+    try:
+        train_dataset = torch.load(train_path)
+        test_dataset = torch.load(test_path)
+    except FileNotFoundError:
+        train_dataset, test_dataset = preProcessData()
+        torch.save(train_dataset, train_path)
+        torch.save(test_dataset, test_path)
+    return train_dataset, test_dataset
 
 CUDA=torch.cuda.is_available()
 device = torch.device("cuda:0" if CUDA else "cpu")
 print(device)
 
-NUM_CLASSES = 43
-IMG_HEIGHT = 32
-IMG_WIDTH = 32
-NUM_EPOCHS = 10
+train_dataset, test_dataset = loadData()
 
-print("Loading data...")
-train_dataset = preProcessData('data/GTSRB/train-data/')
-test_dataset = preProcessData('data/GTSRB/test-data', "test")
-
-runExperiments()
+#print("Running experiments...")
+#runExperiments()
+#createModel(batch_size=4, learning_rate=0.001, momentum=0.9, dropout=0.75)
+#optimizeModel()
--- Neural-Net/net.py
+++ Neural-Net/net.py
@@ -3,51 +3,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class AlexNet(nn.Module):
-    def __init__(self, num_classes=10):
-        super(AlexNet, self).__init__()
-        self.pool = nn.MaxPool2d(kernel_size=3, stride=2) 
-
-        self.conv1 = nn.Conv2d(3, 96, kernel_size=11, stride=4)
-        self.conv2 = nn.Conv2d(96, 256, kernel_size=5, stride=1, padding='same')
-        self.conv3 = nn.Conv2d(256, 384, kernel_size=3, padding='same')
-        self.conv4 = nn.Conv2d(384, 384, kernel_size=3, padding='same')
-        self.conv5 = nn.Conv2d(384, 256, kernel_size=3, padding='same')
-
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 512)
-        self.fc3 = nn.Linear(512, num_classes)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.pool(x)
-
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.pool(x)
-
-        x = self.conv3(x)
-        x = F.relu(x)
-        
-        x = self.conv4(x)
-        x = F.relu(x)        
-        
-        x = self.conv5(x)
-        x = F.relu(x)
-        x = self.pool(x)
-
-        x = torch.flatten(x, 1) #Flatten
-
-        x = self.fc1(x)
-        x = F.relu(x)
-
-        x = self.fc2(x)
-        x = F.relu(x)
-
-        x = self.fc3(x)
-        return F.log_softmax(x, dim=1)
-    
 class LeNet(nn.Module):
     def __init__(self, num_classes=43):
         super(LeNet, self).__init__()
@@ -59,6 +14,8 @@ class LeNet(nn.Module):
         self.fc1 = nn.Linear(400, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_classes)
+        self.dropout = nn.Dropout(0.25)
+
 
     def forward(self, x):
         x = self.conv1(x)
@@ -73,9 +30,55 @@ class LeNet(nn.Module):
 
         x = self.fc1(x)
         x = F.relu(x)
+        x = self.dropout(x)
 
         x = self.fc2(x)
         x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc3(x)
+        return F.log_softmax(x, dim=1)
+    
+class VGNet(nn.Module):
+    def __init__(self, num_classes=43, dropout=0):
+        super(VGNet, self).__init__()
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2) 
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+
+        self.fc1 = nn.Linear(256*4*4, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, num_classes)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        #Block 1
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.max_pool(x)
+
+        #Block 2
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.max_pool(x)
+
+        #Block 3
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.max_pool(x)
+
+        x = torch.flatten(x, 1) #Flatten
+
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.dropout(x)
 
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
